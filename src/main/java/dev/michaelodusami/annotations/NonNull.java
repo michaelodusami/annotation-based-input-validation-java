7bc5df206c51543f0c814fb2b4126bceef8b4a3f
Documentayshin <kyrmikee@gmail.com>
2024-12-16 16:07:56 -0500

    feat: create NonNull.java

---
+++ src/main/java/dev/michaelodusami/annotations/NonNull.java
@@ -0,0 +1,12 @@
+package dev.michaelodusami.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface NonNull {
+    String message () default "This field cannot be null.";
+}
